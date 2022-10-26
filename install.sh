23371948f9220701ca8e1efb6e67eae4fa2a3fc9
Mirko Brombin <send@mirko.pm>
2022-10-26 16:24:25 +0200

    update for new repo

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This script install or the utilities in the current directory.
 
+sudo almost enter rw
+
 for file in $(ls -1); do
     if [ "$file" != "install.sh" ]; then
         sudo rm -f /usr/bin/$file
@@ -8,3 +10,5 @@ for file in $(ls -1); do
         sudo chmod +x /usr/bin/$file
     fi
 done
+
+sudo almost enter ro
